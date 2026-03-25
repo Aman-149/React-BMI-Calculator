61b372a35e9eb667dce750d7bdd6dd127e33fba4
Amanuel Fikadu <amanuelfikadu23971@gmail.com>
2026-03-25 11:34:43 +0300

    taking input

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,28 @@ import React from 'react'
 
 export const App = () => {
   return (
-    <div>App</div>
+  <div className="main">
+    <div className="main">
+      <h2 className='uppername'>BMI Calculator</h2>
+    </div>
+    <div className='inner'>
+      <form className="form">
+        <div className="form-group">
+          <label htmlFor="height">Height (cm):</label>
+          <input type="number" id="height" name="height" />
+        </div>
+        <div className="form-group">
+          <label htmlFor="weight">Weight (kg):</label>
+          <input type="number" id="weight" name="weight" />
+        </div>
+        <button type="submit">Calculate BMI</button>
+      </form>
+    </div>
+
+    <div>
+      
+    </div>      
+  </div>
   )
 }
+export default App
