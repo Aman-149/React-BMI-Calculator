c29760a0f6fa09db2704cdcf25c4aad2da47356e
Amanuel Fikadu <amanuelfikadu23971@gmail.com>
2026-03-28 00:26:59 +0300

    better ui and functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,59 @@
-import React from 'react'
+import React,{ useState } from 'react'
 
 export const App = () => {
+  const [height,setHeight]=useState('')
+  const [weight,setWeight]=useState('')
+  const [bmi,setbmi]=useState(null)
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    if(!height || !weight){
+      alert('Please enter both height and weight')
+      return
+    }
+  const h= height/100
+  const value =(weight/(h*h)).toFixed(2)
+  setbmi(value)
+}
+const handleReset =() => {
+  setHeight('')
+  setWeight('')
+  setbmi(null)
+}
+
   return (
   <div className="main">
     <div className='inner'>
-        <div className="main1">
         <h2 className='uppername'>BMI Calculator</h2>
-    </div>
-      <form className="form">
+      <form className="form" onSubmit={handleSubmit}>
         <div className="form-group">
           <label htmlFor="height">Height (cm):</label>
-          <input type="number" id="height" name="height" placeholder='170cm' />
+          <input 
+          type="number" 
+          value={height}
+          id='height'
+          onChange={(e) => setHeight(e.target.value)}
+          name="height" 
+          placeholder='170cm' />
         </div>
         <div className="form-group">
           <label htmlFor="weight">Weight (kg):</label>
-          <input type="number" id="weight" name="weight" placeholder='60Kg' />
+          <input 
+          type="number" 
+          value={weight}
+          onChange={(e) => setWeight(e.target.value)}
+          id="weight" 
+          name="weight" 
+          placeholder='60Kg' />
         </div> 
         <div className="buttons">
         <button className="calculate" type="submit">Calculate</button>
-        <button className="reset" type="reset">Clear</button>
+        <button className="reset" type="button" onClick={handleReset}>Clear</button>
         </div>
-      </form>
+        {bmi && <h3>Your BMI is: {bmi}</h3>}
+      </form> 
     </div>
-
-    <div>
-      
     </div>      
-  </div>
   )
 }
 export default App
