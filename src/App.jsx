2963d994ac2d9d4c02cb606df91ba6c7629c9a84
Amanuel Fikadu <amanuelfikadu23971@gmail.com>
2026-03-27 23:06:00 +0300

    adding ui

--- src/App.jsx
+++ src/App.jsx
@@ -3,20 +3,23 @@ import React from 'react'
 export const App = () => {
   return (
   <div className="main">
-    <div className="main">
-      <h2 className='uppername'>BMI Calculator</h2>
-    </div>
     <div className='inner'>
+        <div className="main1">
+        <h2 className='uppername'>BMI Calculator</h2>
+    </div>
       <form className="form">
         <div className="form-group">
           <label htmlFor="height">Height (cm):</label>
-          <input type="number" id="height" name="height" />
+          <input type="number" id="height" name="height" placeholder='170cm' />
         </div>
         <div className="form-group">
           <label htmlFor="weight">Weight (kg):</label>
-          <input type="number" id="weight" name="weight" />
+          <input type="number" id="weight" name="weight" placeholder='60Kg' />
+        </div> 
+        <div className="buttons">
+        <button className="calculate" type="submit">Calculate</button>
+        <button className="reset" type="reset">Clear</button>
         </div>
-        <button type="submit">Calculate BMI</button>
       </form>
     </div>
 
